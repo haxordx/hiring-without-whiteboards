a50f072f2754efe403d5a2edbd8fc80a4a00abf4
Ian Jennings <ian@meetjennings.com>
2021-04-19 21:32:15 -0500

    generate csv

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = {};
+const fs = require('fs');
+const _ = require('lodash');
+
+let file = fs.readFileSync('./README.md', 'utf-8');
+let lines = file.split('\n');
+
+let csvHeader = 'Name, URL'
+
+let csv = [csvHeader];
+_.each(lines, (line) => {
+    let parts = line.split('|');
+    
+    let webName = parts[0].split('](');
+
+    let name = webName[0].replace('- [', '');
+
+    url = '';
+    if (webName[1]) {
+        url = webName[1].replace(') ', '');
+    }
+   
+
+    csv.push(`${name}, ${url}`);
+
+});
+
+fs.writeFileSync('without-whiteboards.csv', csv.join('\n'), 'utf-8');
